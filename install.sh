6bc7bccd302b6103cc40a49d8a36f5cc2da2f8c5
khanhdang <khanhdangquocag97@gmail.com>
2019-01-16 17:46:20 +0700

    Backup at Wed Jan 16 17:46:20 +07 2019

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sf "$DOTFILES_DIR/.gitcfg/.gitconfig" ~
 ln -sf "$DOTFILES_DIR/.gitcfg/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/.zsh/.zshrc" ~
-ln -sf "$DOTFILES_DIR//.zsh.zsh_exports" ~
+ln -sf "$DOTFILES_DIR/.zsh/.zsh_exports" ~
 ln -sf "$DOTFILES_DIR/.zsh/.zsh_aliases" ~
-ln -sf "$DOTFILES_DIR/.vim" "$HOME/.vim"
+ln -sf "$DOTFILES_DIR/.vim" ~
+ln -sf "$DOTFILES_DIR/.vim/vimrc" ~/.vimrc
