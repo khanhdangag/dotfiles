11c0e892e734e1d6db10907503d7721fb9c14b3d
khanhdang <khanhdangquocag97@gmail.com>
2019-01-16 18:34:18 +0700

    Backup at Wed Jan 16 18:34:18 +07 2019

--- backup.sh
+++ backup.sh
@@ -1,9 +1,4 @@
 git pull origin master
-while read f; do
-    echo "Backup file $f..."
-    cp -rf $HOME/$f ./
-done < .dotfiles
-echo "Backup files done, please commit!"
 
 git add . -A
 git commit -m "Backup at $(date)"
