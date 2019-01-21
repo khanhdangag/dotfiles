0e687bce92ed9048bceb8c5f14f35f0db0f28346
khanhdang <khanhdangquocag97@gmail.com>
2019-01-21 13:43:50 +0700

    Backup at Mon Jan 21 13:43:50 +07 2019

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-list_package="htop python3-pip python3-dev"
+list_package="htop python3-pip python3-dev snapd"
 for i in $list_package; do
     echo "installing $i"
+    echo "------------------------------------------------------------"
     sudo apt install $i
 done
